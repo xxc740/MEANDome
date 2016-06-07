7bcd12a091c1baaa1fc5fc4bf280250768264f82
xxc740 <740415505@qq.com>
2016-06-07 23:41:02 +0800

    start

---
+++ BookClub/BookClub/app_server/controllers/home.js
@@ -0,0 +1,11 @@
+﻿module.exports.index = function(req, res) {
+    res.render('index', { title: 'Index' });
+};
+
+module.exports.books = function(req, res) {
+    res.render('books', { title: 'Books' });
+};
+
+module.exports.about = function(req, res) {
+    res.render('about', { title: 'About' });
+};
---
+++ BookClub/BookClub/app_server/routes/index.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+var router = express.Router();
+var homeController = require('../controllers/home');
+
+/* GET home page. */
+//router.get('/', function (req, res) {
+//    res.render('index', { title: 'BookClub' });
+//});
+
+router.get('/', homeController.index);
+router.get('/about', homeController.about);
+router.get('/books',homeController.books);
+
+module.exports = router;
